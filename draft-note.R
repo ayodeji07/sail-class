9f549e4d24a763ff70ca41beb24f90bcf330e670
Nurudeen Abdulsalaam <olaitansalaam2012@gmail.com>
2024-06-20 11:08:32 +0100

    load data as excel

--- draft-note.R
+++ draft-note.R
@@ -3,13 +3,26 @@ setwd("C:/Users/Open User/Desktop/Nurudeen-DS/SAIL-Project")
 
 
 ## Load Library
-library(readr)
+#library(readr)
+library(tidyverse)
+install.packages("readxl")
 
 ### Read Metadata
-
-Metadata_Country <- read_csv("Metadata_Country_API_SM.POP.NETM_DS2_en_csv_v2_434367.csv")
-Metadata_Indicator <- read_csv("Metadata_Indicator_API_SM.POP.NETM_DS2_en_csv_v2_434367.csv")
-
+Metadata_Country <- read_excel("API_SM.POP.NETM_DS2_en_excel_v2_424013.xls", sheet = 2)
+Metadata_Indicator <- read_excel("API_SM.POP.NETM_DS2_en_excel_v2_424013.xls", sheet = 3)
 
 View(Metadata_Country)
-View(Metadata_Indicator)
+#View(Metadata_Indicator)
+
+
+## Import data
+# Install and load readxl if not already installed
+
+library(readxl)
+my_data <- read_excel("API_SM.POP.NETM_DS2_en_excel_v2_424013.xls", sheet = 1, skip = 3)
+
+# Select columns that are not "Indicator Name" and "Indicator Code"
+my_data <- my_data %>%
+  select(-c(`Indicator Name`, `Indicator Code`))
+
+View(my_data)
