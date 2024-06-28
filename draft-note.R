bf0eeb4a16072d7111e3b4bb8f4e97cb9f98c129
Ayodeji Akande <ayodejiakande2107@gmail.com>
2024-06-28 17:05:24 +0100

    Standardize the country names and create a map

--- draft-note.R
+++ draft-note.R
@@ -1,6 +1,6 @@
 ## Set working directory
 #Ayodeji
-#setwd("C:/Users/Open user/Desktop/SAIL-DS")
+setwd("C:/Users/Open user/Desktop/SAIL-DS")
 #Nurudeen
 #setwd("C:/Users/Open User/Desktop/Nurudeen-DS/SAIL-Project")
 
@@ -13,8 +13,15 @@ pacman::p_load(
   esquisse,
   readxl,
   skimr,
-  
+  sf,
+  rnaturalearth,
+  janitor,
+  tidygeocoder,
+  rnaturalearth
 )
+
+#install.packages("rnaturalearthdata")
+#library(rnaturalearth)
 
 ### Read Metadata
 Metadata_Country <- read_excel("API_SM.POP.NETM_DS2_en_excel_v2_424013.xls", sheet = 2)
@@ -33,6 +40,7 @@ wrangle_data <- function(my_data){
   # Select columns that are not "Indicator Name" and "Indicator Code"
   my_data <- my_data %>%
     select(-c(`Indicator Name`, `Indicator Code`, `Country Code`))
+    
   
   # Create a vector with the names of African countries
   african_countries <- c(
@@ -48,19 +56,44 @@ wrangle_data <- function(my_data){
     "Uganda", "Zambia", "Zimbabwe"
   )
   
+
   ### Filtered african countries
   filtered_data <- my_data %>% filter(`Country Name` %in% african_countries)
   filtered_data <- filtered_data %>% arrange(`Country Name`)
+
+  
+  # Standardized names used by ne_countries()
+  standard_names <- c(
+    "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", 
+    "Cabo Verde", "Cameroon", "Central African Republic", "Chad", "Comoros", 
+    "Congo, Dem. Rep.", "Congo, Rep.", "Djibouti", "Egypt, Arab Rep.", 
+    "Equatorial Guinea", "Eritrea", "Eswatini", "Ethiopia", "Gabon", 
+    "Gambia, The", "Ghana", "Guinea", "Guinea-Bissau", "Côte d'Ivoire", 
+    "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", 
+    "Mali", "Mauritania", "Mauritius", "Morocco", "Mozambique", 
+    "Namibia", "Niger", "Nigeria", "Rwanda", "São Tomé and Príncipe", 
+    "Senegal", "Seychelles", "Sierra Leone", "Somalia", "South Africa", 
+    "South Sudan", "Sudan", "Tanzania", "Togo", "Tunisia", "Uganda", 
+    "Zambia", "Zimbabwe"
+  )
+  
+  filtered_data$`Country Name` <- standard_names
+  names(filtered_data)[names(filtered_data) == "Country Name"] <- "country_name"
+  
   
   return(filtered_data)
 }
+
+
+
+view(filtered_data)
 
 transpose_data <- function(filtered_data){
   # Transpose the data frame
   tibbled_data <- tibble(filtered_data)
   transposed_data <- tibbled_data %>%
-    pivot_longer(cols = -`Country Name`, names_to = "year", values_to = "Value") %>%
-    pivot_wider(names_from = `Country Name`, values_from = Value)
+    pivot_longer(cols = -country_name, names_to = "year", values_to = "Value") %>%
+    pivot_wider(names_from = country_name, values_from = Value)
   
   # Append "-12-31" to each year to represent December 31st of that year
   transposed_data$date <- as.Date(paste0(transposed_data$year, "-1-1"))
@@ -76,7 +109,7 @@ transpose_data <- function(filtered_data){
 
 filtered_data <- wrangle_data("API_SM.POP.NETM_DS2_en_excel_v2_424013.xls")
 filtered_data$total <- filtered_data %>% 
-  select(-c(`Country Name`)) %>% 
+  select(-c(country_name)) %>% 
   rowSums()
 view(filtered_data)
 
@@ -91,7 +124,7 @@ esquisse::esquisser(trans_data)
 
 # Test net migration for all the countries
 ggplot(filtered_data) +
-  aes(x = `Country Name`, y = `total`) +
+  aes(x = country_name, y = `total`) +
   geom_col(fill = "#112446") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90L)) +
@@ -139,10 +172,37 @@ for (country in countries) {
 
 
 ggplot(filtered_data) +
-  aes(x = `Country Name`, y = `1983`) +
+  aes(x = country_name, y = `1983`) +
   geom_col(fill = "#112446") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90L)) +
   labs(title = "Nigeria 1983 Net Migration in Comparison to Other African Countries", y = "Net Migration in Millions")
+
+
+# Load world map data for Africa
+world <- ne_countries(scale = "medium", continent = "Africa", returnclass = "sf")
+
+#Geocode filtered data to get long and lat of the countries
+geocoded_filtered_data <- filtered_data %>% 
+  
+  # geocode Address to lat/long
+  tidygeocoder::geocode(
+    address = country_name,
+    method = "osm"
+  )
+
+view(geocoded_filtered_data)
+
+# Merge with world map data
+merged_data <- left_join(world, geocoded_filtered_data, by = c("name" = "country_name"))
+
+# Plot the map
+ggplot() +
+  geom_sf(data = merged_data, aes(fill = total)) +
+  scale_fill_viridis_c(name = "Net Migration", option = "plasma") +
+  theme_minimal() +
+  labs(title = "Net Migration in African Countries") +
+  # Adding country labels
+  geom_text(data = merged_data, aes(label = name, x = long, y = lat), size = 3.5, color = "black", check_overlap = TRUE, nudge_y = 0.5)
 
 
